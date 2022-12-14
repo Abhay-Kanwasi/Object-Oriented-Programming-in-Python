edd2340da59ad7e785f1958206bd31177abb1522
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2022-12-14 22:00:46 +0530

    improving

--- Student Management System/main.py
+++ Student Management System/main.py
@@ -16,11 +16,57 @@ class Student:
         print("Marks2 : ",ob.m2)
     
     def search(self,rn):
-        for i in range(ls.__len__()):
+        for i in range(len(ls)):
             if(ls[i].rollno == rn):
                 return i
+
     
     def delete(self,rn):
         i = obj.search(rn)
         del ls[i]
+    
+    def update(self,rn,No):
+        i = obj.search(rn)
+        roll = No
+        ls[i].rollno = roll
+
+ls = []
+obj = Student('',0,0,0)
+
+print("\nOperation Used ")
+print("\n1. Accept student details\n2. Display student details\n3. Search details of a student.\n4. Delete details of the student\n5. Update studnet details\n6. Exit")
+while True:
+    ch = int(input("Enter your choice : "))
+    if(ch == 1):
+        name = input("Enter the name : ")
+        rollno = int(input("Enter the roll no. : "))
+        marks1 = int(input("Enter your marks in first subject : "))
+        marks2 = int(input("Enter your marks in secod subject : "))
+        print("Saved")
+        obj.accept(name,rollno,marks1,marks2)
+    elif(ch == 2):
+        print("\n")
+        print("List of Students\n")
+        for i in range(ls.__len__()):
+            obj.display(ls[i])
+    elif(ch==3):
+        rn = int(input("Enter the roll no. : "))
+        Student.search(rn)
+        print("Student found")
+
+    elif(ch == 4):
+        obj.delete(2)
+        print(ls.__len__())
+        print("List after deletion")
+        for i in range(ls.__len__()):
+            obj.display(ls[i])
+    elif(ch == 5):
+        obj.update(3,2)
+        print(ls.__len__())
+        print("List after updation")
+        for i in range(ls.__len__()):
+            obj.display(ls[i])
+
+    else:
+        print("Thank You!")
     
